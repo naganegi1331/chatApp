43f3438f94d20f7755dedd611ef73b24fb794784
Hiroki Kashihara <himawarisun1331@gmail.com>
2025-04-11 19:33:59 +0900

    Initial Commit

---
+++ chatApp/chatAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  chatAppApp.swift
+//  chatApp
+//
+//  Created by Hiroki Kashihara on 2025/04/11.
+//
+
+import SwiftUI
+
+@main
+struct chatAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
